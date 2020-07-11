dab5f978826295d32cb056977dbda2a9b6b30cb7
bboti199 <b.boti199@yahoo.com>
2020-07-11 20:56:36 +0300

    Add jwt authentication and custom UserAccessor class

--- NoteKeeper.Api/Startup.cs
+++ NoteKeeper.Api/Startup.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +14,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using NoteKeeper.DataAccess;
+using NoteKeeper.Infrastructure.Interfaces;
+using NoteKeeper.Infrastructure.Security;
 
 namespace NoteKeeper.Api
 {
@@ -32,6 +38,33 @@ namespace NoteKeeper.Api
                     .UseNpgsql(Configuration.GetConnectionString("DevPostgres"))
                     .UseSnakeCaseNamingConvention();
             });
+
+            services.AddAuthorization()
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    var jwtSettings = Configuration.GetSection("JwtSettings").Get<JwtConfiguration>();
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Issuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret)),
+                        ClockSkew = TimeSpan.Zero,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true
+                    };
+                });
+
+            services.Configure<JwtConfiguration>(Configuration.GetSection("JwtSettings"));
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
             
             services.AddControllers();
         }
@@ -47,6 +80,7 @@ namespace NoteKeeper.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
